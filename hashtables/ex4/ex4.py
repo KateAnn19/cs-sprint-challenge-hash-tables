544cdd3420210f5c6dcb3fee3a2d104787c0eb2b
kate mcgee <mcgeehee@gmail.com>
2020-09-04 10:05:49 -0600

    ex4 working

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,10 +2,26 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
+    nums_dict = {}
+    nums_list = []
     # Your code here
-
-    return result
+    for i in a:
+        pos = abs(i)
+        if pos not in nums_dict:
+            nums_dict[pos] = 1
+        else:
+            nums_dict[pos] += 1
+            if pos not in nums_list:
+                nums_list.append(pos)
+               
+    return nums_list
 
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+
+
+    # input is a list
+    # output is a list of positive integers that had corresponding negative numbers 
+
+    # loop over list and check if 
